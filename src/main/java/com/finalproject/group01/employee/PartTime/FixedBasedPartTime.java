d0dab149839b0cfae4e01f64f132d5c10f873bc3
Giselle Tavares <gitavares@hotmail.com>
2018-10-17 09:41:55 -0400

    implementend the core of all classes. Map for Payroll not finished yet

--- src/main/java/com/finalproject/group01/employee/PartTime/FixedBasedPartTime.java
+++ src/main/java/com/finalproject/group01/employee/PartTime/FixedBasedPartTime.java
@@ -7,11 +7,44 @@ public class FixedBasedPartTime extends PartTime {
     private double fixedAmount;
     private double rateTimesHoursWorked;
 
-    public FixedBasedPartTime(String name, int age, Vehicle vehicle, double rate, double hoursWorked) {
+    public FixedBasedPartTime(String name, int age, Vehicle vehicle, double rate, double hoursWorked, double fixedAmount) {
         super(name, age, vehicle, rate, hoursWorked);
+        this.fixedAmount = fixedAmount;
     }
 
+    public double getFixedAmount() {
+        return fixedAmount;
+    }
 
-//    rateTimesHoursWorked = PartTime().calcEarnings()
+    public void setFixedAmount(double fixedAmount) {
+        this.fixedAmount = fixedAmount;
+    }
+
+    public double getRateTimesHoursWorked() {
+        return rateTimesHoursWorked;
+    }
+
+    public void setRateTimesHoursWorked(double rateTimesHoursWorked) {
+        this.rateTimesHoursWorked = rateTimesHoursWorked;
+    }
+
+    @Override
+    public double calcEarnings() {
+        rateTimesHoursWorked = super.calcEarnings();
+        return rateTimesHoursWorked + this.fixedAmount;
+    }
+
+    @Override
+    public String printMyData() {
+        String message = super.printMyData();
+        message += "Fixed Amount\n";
+        message += "- Rate: " + this.getRate() + "\n";
+        message += "- Hours Worked: " + this.getHoursWorked()+ " \n";
+        message += "- Fixed Amount: " + this.fixedAmount + "\n";
+        message += "- Earnings: " + calcEarnings() + "(" + this.rateTimesHoursWorked + " + " + this.fixedAmount + ")\n";
+        message += "************************************\n";
+        return message;
+
+    }
 
 }
--- src/main/java/com/finalproject/group01/payroll/Payroll.java
+++ src/main/java/com/finalproject/group01/payroll/Payroll.java
@@ -1,19 +1,77 @@
 package com.finalproject.group01.payroll;
 
+import com.finalproject.group01.employee.Employee;
 import com.finalproject.group01.utils.IPrintable;
 
-import java.util.Dictionary;
+import java.util.Map;
 
 public class Payroll implements IPrintable {
 
-    private Dictionary listOfEmployeesOnPayroll;
+    private Map<Integer, Employee> listOfEmployeesOnPayroll;
     private double totalPayroll;
 
-//    var listOfEmployeesOnPayroll: [Int: Employee]
+    public Payroll() {
+    }
 
+    public Payroll(Map<Integer, Employee> listOfEmployeesOnPayroll, double totalPayroll) {
+        this.listOfEmployeesOnPayroll = listOfEmployeesOnPayroll;
+        this.totalPayroll = totalPayroll;
+    }
+
+    public void saveEmployeeOnPayroll(Employee employee) {
+        int id = listOfEmployeesOnPayroll.size();
+        listOfEmployeesOnPayroll.put(id, employee);
+    }
+
+    public Double calcTotalPayroll() {
+
+        for(Employee employee: listOfEmployeesOnPayroll.values()){
+            this.totalPayroll += employee.calcEarnings();
+        }
+        return totalPayroll;
+    }
 
     @Override
     public String printMyData() {
-        return null;
+        String payrollData = "";
+
+        for(Employee employee: listOfEmployeesOnPayroll.values()){
+            System.out.println(employee.printMyData());
+            payrollData += employee.printMyData();
+        }
+
+        String message = "Total of employees: " + listOfEmployeesOnPayroll.size() + "\n";
+        message += "TOTAL PAYROLL: " + calcTotalPayroll() + " Canadian Dollars";
+        message += "";
+
+
+
+
+
+//        for employee in listOfEmployeesOnPayroll {
+//            print(employee.value.printMyData())
+//            payrollData += employee.value.printMyData()
+//        }
+
+//        String message = "Total of employees: \(listOfEmployeesOnPayroll.count)\n"
+//        message += "TOTAL PAYROLL: \(calcTotalPayroll().currency) Canadian Dollars"
+//        message += ""
+
+        // --------------------------
+        // saving results on TXT file
+//        let fileName = "payrollTotal"
+//        let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+//        let fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
+//        payrollData += message
+//        do {
+//            // Write to the file
+//            try payrollData.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+//        } catch let error as NSError {
+//            print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
+//        }
+
+//        message += "\n\nThe Payroll was saved on: \(DocumentDirURL)\(fileName).txt"
+
+        return message;
     }
 }
